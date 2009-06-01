510f300ad6f8a0c76a116a3a8bab4c827db57cd5
Sean Clark <sc1245@messiah.edu>
2009-06-01 15:21:14 -0400

    tabs -> spaces

--- timerobjectClassTest.php
+++ timerobjectClassTest.php
@@ -22,23 +22,23 @@ class timerobjectClass extends PHPUnit_Framework_TestCase
     }
     
     public function testStartTimerReturnsFloat() {
-		$this->t->_starttime = 'String';
+        $this->t->_starttime = 'String';
         $this->t->startTimer();
         $this->assertType(float, $this->t->_starttime);
     }
-	
-	public function testStopTimeReturnsFloat() {
-		$this->t->_endtime = 'String';
-		$this->t->stopTimer();
-		$this->assertType(float, $this->t->_endtime);
-	}
     
-	public function test_SetElapsedTime() {
-		$this->t->_starttime = 1234.56;
-		$this->t->_endtime = 1234.66;
-		$this->t->_setElapsedTime();
-		$this->assertEquals(.10, $this->t->_elapsedtime);
-	}
+    public function testStopTimeReturnsFloat() {
+        $this->t->_endtime = 'String';
+        $this->t->stopTimer();
+        $this->assertType(float, $this->t->_endtime);
+    }
+    
+    public function test_SetElapsedTime() {
+        $this->t->_starttime = 1234.56;
+        $this->t->_endtime = 1234.66;
+        $this->t->_setElapsedTime();
+        $this->assertEquals(.10, $this->t->_elapsedtime);
+    }
 }
 
 ?>
