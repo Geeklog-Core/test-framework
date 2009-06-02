a55e2e311a67055c3eeec3a9cfaf88e3f3254659
Sean Clark <sc1245@messiah.edu>
2009-06-02 11:54:01 -0400

    timerobjectClassTestfinished

--- timerobjectClassTest.php
+++ timerobjectClassTest.php
@@ -11,8 +11,10 @@ require_once '../../../geeklog/system/classes/timer.class.php';
 class timerobjectClass extends PHPUnit_Framework_TestCase 
 {
     private $t;
+	
     protected function setUp() {
         $this->t = new timerobject;
+		$this->prec = 0.0001;
     }    
     
     public function testSetPrecisionReturnsDecimalPlaces() {
@@ -20,13 +22,13 @@ class timerobjectClass extends PHPUnit_Framework_TestCase
         $this->t->setPrecision(4);
         $this->assertEquals(4, $this->t->_precision);
     }
-    
+
     public function testStartTimerEqualsDummy() {
         $mtime = microtime();
         $mtime = explode(' ', $mtime);
         $var1 = $mtime[1] + $mtime[0];
         $this->t->startTimer();
-        $this->assertEquals(0, bccomp($var1, $this->t->_starttime));
+        $this->assertEquals($var1, $this->t->_starttime, '', $this->prec);
     }
     
     public function testStartTimerReturnsFloat() {
@@ -34,28 +36,69 @@ class timerobjectClass extends PHPUnit_Framework_TestCase
         $this->t->startTimer();
         $this->assertType(float, $this->t->_starttime);
     }
-    
-    public function testStopTimeReturnsFloat() {
-        $this->t->_endtime = 'String';
-        $this->t->stopTimer();
-        $this->assertType(float, $this->t->_endtime);
+	
+	public function testStopTimerEqualsDummy() {
+		$mtime = microtime();
+        $mtime = explode(' ',$mtime);
+        $var1 = $mtime[1] + $mtime[0];
+		$this->t->stopTimer();
+		$this->assertEquals($var1, $this->t->_endtime, '', $this->prec);
+	}
+	
+	public function testStopTimerReturnsCorrectDefaultDegreeOfPrecision() {
+		$mtime = microtime();
+        $mtime = explode(' ',$mtime);
+        $var1 = $mtime[1] + $mtime[0];
+		$var2 = sprintf('%.2f', $var1);		
+		$this->assertEquals($var2, $this->t->stopTimer());
+	}
+	
+	public function testStopTimerReturnsCorrectDefinedDegreeOfPrecision() {
+		$mtime = microtime();
+        $mtime = explode(' ',$mtime);
+        $var1 = $mtime[1] + $mtime[0];
+		$var2 = sprintf('%.3f', $var1);		
+		$this->t->_precision = 3;
+		$this->assertEquals($var2, $this->t->stopTimer());
+	}
+
+    public function testStopTimerReturnsString() {
+		$this->t->_starttime = .56;
+        $this->assertType(string, $this->t->stopTimer());
     }
     
+	public function testRestartResets_Endtime() {
+		$this->t->restart();
+		$this->assertEquals('', $this->t->_endtime);
+	}
+	
+	public function testRestartRedefines_Starttime(){
+		$this->t->restart();		
+		$mtime = microtime();
+        $mtime = explode(' ', $mtime);
+        $mtime = $mtime[1] + $mtime[0];
+		$this->assertEquals($mtime, $this->t->_starttime, '', $this->prec);
+	}
+	
+	public function testGetElapsedTimeReturnsDefaultDegreeofPrecision() {
+		$this->t->_elapsedtime = .12345;
+		$var1 = sprintf('%.2f', .12345);
+		$this->assertEquals($var1, $this->t->getElapsedTime());
+	}
+	
+	public function testGetElapsedTimeReturnsDefinedDegreeofPrecision() {
+		$this->t->_elapsedtime = .12345;
+		$this->t->_precision = 3;
+		$var1 = sprintf('%.3f', .12345);
+		$this->assertEquals($var1, $this->t->getElapsedTime());
+	}
+	
     public function test_SetElapsedTime() {
         $this->t->_starttime = .56;
         $this->t->_endtime = .66;
         $this->t->_setElapsedTime();
-        $var = bccomp(.10, $this->t->_elapsedtime);
-        $this->assertEquals(0, $var);
+        $this->assertEquals(.10, $this->t->_elapsedtime, '', $this->prec);
     }
-    
-    public function testFloatsCompare() {
-        $var1 = 12.1;
-        $var2 = 12.1;
-        $this->assertEquals($var1, $var2);
-    }
-    
-    
 }
 
 ?>
