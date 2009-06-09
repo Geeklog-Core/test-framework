1c0f8f5308e2042a2a22fcc4601aec9ce4a8a849
Sean Clark <sc1245@messiah.edu>
2009-06-09 13:46:36 -0400

    restructured

--- COM_createImageTest.php
+++ COM_createImageTest.php
@@ -5,10 +5,11 @@
 */
 
 require_once 'PHPUnit/Framework.php';
+require_once 'config.php';
+require_once getPath('public_html').'lib-common.php';
 
 global $_CONF, $_TABLES, $_DB_table_prefix, $_DB_mysqldump_path;
-require_once '../lib-common.php';
- 
+
 class COM_createImageTest extends PHPUnit_Framework_TestCase
 {
     public function testCreateWithHttpUrl()
