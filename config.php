ae3deb4dd2daab45db0223ff36cb4e923d9eaf64
Sean Clark <sc1245@messiah.edu>
2009-06-10 12:28:47 -0400

    New folder structure, failing relative config path

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Configuration for PHPUnit tests
+*/
+
+/**
+* Sets paths for PHPUnit, as tests need to be able to run without
+* an install, but local setups can differ.
+*
+* @param    string  $location    root folder either public_html 
+                                   or outside webroot
+* @return   string  $path       username or empty string
+*
+*/
+
+function getPath($location) {
+    if($location == 'public') {
+        // Should be /path/to/public_html root
+        $path = 'c:/xampplite/htdocs/geeklog/public_html';
+    } elseif($location == 'restricted') {
+        // Should be /path/to/install root
+        $path = 'c:/xampplite/htdocs/geeklog';
+    } elseif($location == 'admin') {
+        // Shohuld be path/to/admin root
+        $path = 'c:/xampplite/htdocs/geeklog/public_html/admin';
+    }
+    return $path;
+}
+
+?>
