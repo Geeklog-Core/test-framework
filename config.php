feab4637427512021ac188e5c2ade6c70c1de576
Sean Clark <sc1245@messiah.edu>
2009-06-19 14:12:48 -0400

    New portable SECURE structure

--- config.php
+++ config.php
@@ -1,37 +1,10 @@
 <?php
 /*
-Configuration for PHPUnit tests
+	Extension of config.php for PHPUnit.
+
+	USER CONFIGURED: This should be path/to/tests/suite/config.php
 */
 
-/**
-* Sets paths for PHPUnit, as tests need to be able to run without
-* an install, but local setups can differ.
-*
-* @param    string  $location   set root folder for public_html, 
-*                                main geeklog install, or admin
-* @return   string  $path       username or empty string
-*
-*/
-
-function getPath($location) {
-    switch($location) {
-        case 'public':
-            // Should be /path/to/public_html root
-            $path = 'c:/xampplite/htdocs/geeklog/public_html';
-            return $path;
-          case 'restricted':
-            // Should be /path/to/core/install root
-            $path = 'c:/xampplite/htdocs/geeklog';
-            return $path;
-        case 'tests':
-            //Should be path/to/testpackage root
-            $path = 'c:/xampplite/htdocs/geeklog/public_html/gsoc-2009-sclark';
-            return $path;
-        case 'admin':
-            // Shohuld be path/to/admin root
-            $path = 'c:/xampplite/htdocs/geeklog/public_html/admin';
-            return $path;
-    }
-}
+require_once 'c:/xampplite/htdocs/geeklog/public_html/tests/tests/config.php';
 
 ?>
