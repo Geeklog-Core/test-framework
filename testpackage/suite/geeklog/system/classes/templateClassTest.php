58c7297017a908b07f813cfa7e20bc4cc83e1f68
Dirk Haun <dirk@haun-online.de>
2010-07-31 19:07:48 +0200

    Test unset_var, get_vars, get_undefined

--- testpackage/suite/geeklog/system/classes/templateClassTest.php
+++ testpackage/suite/geeklog/system/classes/templateClassTest.php
@@ -113,6 +113,58 @@ class templateClass extends PHPUnit_Framework_TestCase
         $this->assertEquals("", $tp2->get_var('test2'));
     }
 
+    public function testUnsetVar() {
+        $tp2 = new Template;
+        $tp2->set_var('test', 'test42');
+        $this->assertEquals("test42", $tp2->get_var('test'));
+        $tp2->unset_var('test');
+        $this->assertEquals("", $tp2->get_var('test'));
+    }
+
+    public function testUnsetVarNotExist() {
+        $tp2 = new Template;
+        $tp2->unset_var('doesnotexist');
+        $this->assertEquals("", $tp2->get_var('doesnotexist'));
+    }
+
+    public function testUnsetVarHash() {
+        $tp2 = new Template;
+        $hash = array('test' => 'test43');
+        $tp2->set_var($hash);
+        $this->assertEquals("test43", $tp2->get_var('test'));
+        $tp2->unset_var('test');
+        $this->assertEquals("", $tp2->get_var('test'));
+    }
+
+    public function testUnsetVarHash2() {
+        $tp2 = new Template;
+        $hash = array('test' => 'test43');
+        $tp2->set_var($hash);
+        $this->assertEquals("test43", $tp2->get_var('test'));
+        $hash2 = array('test');
+        $tp2->unset_var($hash2);
+        $this->assertEquals("", $tp2->get_var('test'));
+    }
+
+    public function testUnsetVarHashNotExist() {
+        $tp2 = new Template;
+        $hash = array('doesnotexist');
+        $tp2->unset_var($hash);
+        $this->assertEquals("", $tp2->get_var('doesnotexist'));
+    }
+
+    public function testUnsetVarHashMultiple() {
+        $tp2 = new Template;
+        $hash = array('test1' => 'test41', 'test2' => 'test42');
+        $tp2->set_var($hash);
+        $this->assertEquals("test41", $tp2->get_var('test1'));
+        $this->assertEquals("test42", $tp2->get_var('test2'));
+        $hash2 = array('test1', 'test2');
+        $tp2->unset_var($hash2);
+        $this->assertEquals("", $tp2->get_var('test1'));
+        $this->assertEquals("", $tp2->get_var('test2'));
+    }
+
     public function testSetRoot() {
         $tp2 = new Template;
         $this->assertTrue($tp2->set_root('.'));
@@ -170,6 +222,14 @@ class templateClass extends PHPUnit_Framework_TestCase
         $this->assertFalse($tp2->set_file($files));
     }
 
+    public function testGetVars() {
+        $tp2 = new Template;
+        $hash = array('test2' => 'test43', 'test1' => 'test42');
+        $tp2->set_var($hash);
+        $vars = $tp2->get_vars();
+        $this->assertEquals($hash, $vars);
+    }
+
     public function testReplaceVariable() {
         $tp2 = new Template(Tst::$tests . 'files/templates');
         $this->assertTrue($tp2->set_file('testfile', 'replace1.thtml'));
@@ -179,6 +239,27 @@ class templateClass extends PHPUnit_Framework_TestCase
         $this->assertEquals('<p>replaced</p>', $replaced);
     }
 
+    public function testGetUndefined() {
+        $tp2 = new Template(Tst::$tests . 'files/templates');
+        $this->assertTrue($tp2->set_file('testfile', 'replace1.thtml'));
+        $undef = $tp2->get_undefined('testfile');
+        $expected = array('test' => 'test'); // a hash of varname/varname pairs
+        $this->assertEquals($expected, $undef);
+    }
+
+    public function testGetUndefinedLoadError() {
+        $tp2 = new Template(Tst::$tests . 'files/templates');
+        // we don't want the error handler to kick in, so:
+        $tp2->halt_on_error = 'no';
+        $this->assertTrue($tp2->set_file('testfile', 'doesnotexist.thtml'));
+        $this->assertFalse($tp2->get_undefined('testfile'));
+    }
+
+    public function testGetUndefinedUnknownVariable() {
+        $tp2 = new Template(Tst::$tests . 'files/templates');
+        $this->assertTrue($tp2->set_file('testfile', 'replace1.thtml'));
+        $this->assertFalse($tp2->get_undefined('doesnotexist'));
+    }
 }
 
 ?>
