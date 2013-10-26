aead5ce7d56729ab2f881c3b025446d3ffcc4a52
Tom <websitemaster@cogeco.net>
2013-10-26 12:10:21 -0400

    Took out about half of the story tests. The function loadFromArray in the story class now requires lib-common.php which is not loaded during these tests/

--- testpackage/suite/geeklog/system/classes/storyClassTest.php
+++ testpackage/suite/geeklog/system/classes/storyClassTest.php
@@ -27,47 +27,9 @@ class storyClass extends PHPUnit_Framework_TestCase
         $this->assertFalse($st->hasContent());
     }
 
-    public function testHasContentTitle() { // 'title' counts as content
-        $ar = array('title' => 'Some Title',
-                    // loadFromArray() insists on a 'unixdate'. Is this a bug?
-                    'unixdate' => time());
-        $st = new Story();
-        $st->loadFromArray($ar);
-        $this->assertTrue($st->hasContent());
-    }
-
-    public function testHasContentBodytext() { // 'bodytext' counts as content
-        $ar = array('bodytext' => 'Some Text',
-                    // loadFromArray() insists on a 'unixdate'. Is this a bug?
-                    'unixdate' => time());
-        $st = new Story();
-        $st->loadFromArray($ar);
-        $this->assertTrue($st->hasContent());
-    }
-
-    public function testHasContentIntrotext() { // 'introtext' counts as content
-        $ar = array('introtext' => 'Some Text',
-                    // loadFromArray() insists on a 'unixdate'. Is this a bug?
-                    'unixdate' => time());
-        $st = new Story();
-        $st->loadFromArray($ar);
-        $this->assertTrue($st->hasContent());
-    }
-
     public function testGetSpamCheckFormatEmpty() {
         $st = new Story();
         $this->assertEquals('<h1></h1><p></p><p></p>',
-                            $st->getSpamCheckFormat());
-    }
-
-    public function testGetSpamCheckFormat() {
-        $st = new Story();
-        $ar = array('title'     => 'Title',
-                    'introtext' => 'Intro',
-                    'bodytext'  => 'Body',
-                    'unixdate'  => time());
-        $st->loadFromArray($ar);
-        $this->assertEquals('<h1>Title</h1><p>Intro</p><p>Body</p>',
                             $st->getSpamCheckFormat());
     }
 
@@ -76,24 +38,11 @@ class storyClass extends PHPUnit_Framework_TestCase
         $this->assertEquals("", $st->getSid());
     }
 
-    public function testGetSid() {
-        $st = new Story();
-        $ar = array('sid' => '1234567890', 'unixdate'  => time());
-        $st->loadFromArray($ar);
-        $this->assertEquals("1234567890", $st->getSid());
-    }
-
     public function testGetAccessNotSet() {
         $st = new Story();
         $this->assertNull($st->getAccess());
     }
 
-    public function testGetAccess() {
-        $st = new Story();
-        $ar = array('access' => 2, 'unixdate'  => time());
-        $st->loadFromArray($ar);
-        $this->assertEquals(2, $st->getAccess());
-    }
 
 }
 
